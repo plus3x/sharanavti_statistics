c2b1559b45c40e0850712387185c0560f6718adf
Vladislav Petrov <electronicchest@gmail.com>
2014-01-20 15:15:33 +0400

    Generate Sessions
    rails g controller Sessions new create destroy

---
+++ test/controllers/sessions_controller_test.rb
@@ -0,0 +1,26 @@
+require 'test_helper'
+
+class SessionsControllerTest < ActionController::TestCase
+  test "should get new" do
+    get :new
+    assert_response :success
+  end
+  
+  test "should login" do
+    dave = users(:client)
+    post :create, name: dave.name, password: 'client'
+    assert_redirected_to main_url
+    assert_equal dave.id, session[:user_id]
+  end
+  
+  test "should fail login" do
+    dave = users(:client)
+    post :create, name: dave.name, password: 'wrong'
+    assert_redirected_to login_url
+  end
+  
+  test "should logout" do
+    delete :destroy
+    assert_redirected_to main_url
+  end
+end
