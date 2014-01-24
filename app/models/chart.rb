a43538b3573f6a287f6cc73d71220476f0546d77
plus3x <electronicchest@gmail.com>
2014-01-24 20:59:12 +0400

    getJSON works

--- app/models/chart.rb
+++ app/models/chart.rb
@@ -2,35 +2,32 @@ class Chart
   def self.game_online(from, to)
     people_on_game = Statistic.select(from: from, to: to)
     game_online = []
-    people_on_game.each_with_index { |people, index| game_online << [(from + 1.minutes * index).to_i * 1000, people] }
+    people_on_game.each_with_index { |people, index| game_online << {x: (from + 1.minutes * index).to_i * 1000, y: people} }
     
     LazyHighCharts::HighChart.new do |f|
-      f.chart type: :area#, animation: 'Highcharts.svg', events: { load: js_set_interval }
+      f.chart type: :area, animation: 'Highcharts.svg',
+              events: {
+                load: %|function() {
+                        var series = this.series[0];
+                        setInterval(function() {
+                            var x = (new Date()).getTime(), // current time
+                                y = Math.random() * 180 + 20;
+                            series.addPoint([x, y], true, true);
+                        }, 60000); // 1 minute
+                    }|.js_code
+              }
 
       f.title    text: 'Статистика количества игроков в игре Шаранавты', x: -20
-      f.subtitle text: "Онлайн #{game_online[-1][1]} шарик(ов). За последнее время пришло #{game_online[-1][1]-game_online[-2][1]}", x: -20
+      f.subtitle text: "Онлайн #{game_online[-1][:y]} шарик(ов). За последнее время пришло #{game_online[-1][:y]-game_online[-2][:y]}", x: -20
 
       f.xAxis type: :datetime, tickInterval: 1.hour.to_i * 1000
       f.yAxis title: {text: 'Количество шариков'}, plot_lines: [{ value: 0, width: 1, color: '#808080'}]
 
       f.tooltip value_suffix: ' шарик(ов)'
-
-      f.legend layout: :vertical, align: :right, verticalAlign: :top, x: -10, y: 100, borderWidth: 0
+      f.legend enabled: false
 
       f.series name: 'Шарики', data: game_online
     end
-  end
-
-  def js_set_integval
-    %| function() {
-      // set up the updating of the chart each second
-      var series = this.series[0];
-      setInterval(function() {
-        var x = (new Date()).getTime(), // current time
-            y = Math.random();
-        series.addPoint([x,y],true,true);
-      }, 1000);
-    }|.js_code
   end
 
   def js_set_interval_with_get_json
@@ -56,32 +53,33 @@ class Chart
     people_on_game.each_with_index { |people, index| game_online << {x: (from + 1.minutes * index).to_i * 1000, y: people} }
 
     LazyHighCharts::HighChart.new do |f|
-      f.chart type: :area, 
-              animation: 'Highcharts.svg', 
-              marginRight: 10, 
-              events: { 
-                load: %|function() {
-                        // set up the updating of the chart each second
-                        var series = this.series[0];
-                        setInterval(function() {
-                            var x = (new Date()).getTime(), // current time
-                                y = Math.random() * 180 + 20;
-                            series.addPoint([x, y], true, true);
-                        }, 60000);
+      f.chart type: :area,
+              animation: 'Highcharts.svg',
+              events: {
+                load: %|
+                    function() {
+                      var series = this.series[0];
+                      setInterval(function() {
+                        var x = (new Date()).getTime(), // current time
+                            y = Math.random() * 180 + 20;
+                        $.getJSON("/get_new_dot", function(data) {
+                          y = 5; // change 5 to 'data'!!!
+                        });
+                        series.addPoint([x, y], true, true);
+                      }, 5000); // 5 second
                     }|.js_code
               }
 
-      f.title text: 'Live random data'
+      f.title    text: 'Статистика количества игроков в игре Шаранавты', x: -20
+      f.subtitle text: "Онлайн #{game_online[-1][:y]} шарик(ов). За последнее время пришло #{game_online[-1][:y]-game_online[-2][:y]}", x: -20
 
-      f.xAxis type: :datetime, tickInterval: 1.hour * 1000
-      f.yAxis title: {text: 'шарики'}, plot_lines: [{ value: 0, width: 1, color: '#808080'}]
+      f.xAxis type: :datetime, tickInterval: 1.hour.to_i * 1000
+      f.yAxis title: {text: 'Количество шариков'}, plot_lines: [{ value: 0, width: 1, color: '#808080'}]
 
       f.tooltip value_suffix: ' шарик(ов)'
-
       f.legend enabled: false
-      f.exporting enabled: false
 
-      f.series name: 'Онлайн', data: game_online
+      f.series name: 'Шарики', data: game_online
     end
   end
 end
