c2094853098f7ac1e893b5a0a71d81f29d8a0294
Vladislav <quick@Shubuntu.(none)>
2014-01-31 18:18:38 +0400

    Chart works

--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -2,13 +2,13 @@ class StatisticsController < ApplicationController
 
   # GET /new_dot
   def new_dot
-    @dot = Statistic.new_dot
+    @dot = API.game_online_dot
     render json: @dot
   end
 
   # GET /game_online
   def game_online
-  	@game_online = Statistic.game_online
+  	@game_online = API.game_online_select( 12.hours.ago, Time.now )
     render json: @game_online
   end
 end
--- test/API_test_server.rb
+++ test/API_test_server.rb
@@ -2,25 +2,17 @@ require 'sinatra'
 require 'json'
 require 'active_support/core_ext/hash/conversions'
 
-# POST /statistics/between?from=2014-01-21+18%3A04%3A11+%2B0400&to=2014-01-27+18%3A04%3A11+%2B0400
-post '/statistics/between' do
-  data = []
-  from = params[:from].to_datetime.to_i
-  to   = params[:to  ].to_datetime.to_i
-  (from..to).step(1.minutes) { |time| data << { x: time, y: Random.rand(200..1000) } }
+# POST /api/stat.online.get?date_from=2013-10-15&date_to=2013-10-16
+post '/api/stat.online.get' do
+  from = params[:date_from].to_datetime.to_i
+  to   = params[:date_to  ].to_datetime.to_i
 
-  puts "Array size: #{data.size}. First element: #{data.first}."
+  data = []
+  (from..to).step(1.minutes) { |time| data << Random.rand(200..1000) }
+
+  puts "Array size: #{data.size}. Last element: #{data.last}."
 
   content_type :json
   data.to_json
-end
-
-# GET /statistics/new_dot
-get '/statistics/new_dot' do
-  dot = { x: Time.now, y: Random.rand(200..1000) }
-
-  puts "Dot: #{dot}"
-
-  content_type :json
-  dot.to_json
+  # OUTPUT: [30, 1, 2]
 end
