e2952d9c41ed91ee496836e1eb219e33130bd970
plus3x <electronicchest@gmail.com>
2014-01-31 13:35:54 +0400

    Chart with currency

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -1,2 +1,10 @@
 class ChartsController < ApplicationController
+
+  # GET /chart/game_online_select?from=&to=
+  def game_online_select
+    from = params[:from]
+    to   = params[:to]
+    to = Time.now if params[:to].to_datetime > Time.now
+    @game_online = Chart.game_online( from, to )
+  end
 end
--- app/models/chart.rb
+++ app/models/chart.rb
@@ -1,7 +1,100 @@
 class Chart
 
   # OPTIMIZE - move java script to assets(chart.js.coffee)
-  def self.game_online
+  def self.game_online(from, to)
+    game_online = API.game_online_select(from: from, to: to)
+
+    return nil unless game_online
+
+    minutes = []
+    ( from.to_i .. to.to_i ).step(1.minute.to_i) { |e| minutes << (e * 1000) }
+    game_online = game_online.last( minutes.count )
+    data = minutes.zip(game_online)
+
+    LazyHighCharts::HighChart.new :game_online do |f|
+      f.chart type: :area,
+              zoom_type: :x,
+              animation: 'Highcharts.svg',
+              events: {
+                load: %|
+                    function() {
+                      var series = this.series[0],
+                          chart = this,
+                          color = '#0a0',
+                          series_length = series.points.length,
+                          last_point    = series.points[series_length - 1],
+                          latest_point  = series.points[series_length - 2],
+                          difference = last_point.y - latest_point.y;
+
+                      // Update currency
+                      if (difference < 0) { color = '#c00'; }
+                      $("#current").text( last_point.y );
+                      $("#difference").text( difference );
+                      $("#difference").css({ color: color });
+
+                      // Mark last point
+                      last_point.update({ marker: { enabled: true  } });
+
+                      setInterval(function() {
+                        color = '#0a0';
+                        difference = 0;
+
+                        $.getJSON("/new_dot" )
+                          .fail( function() { console.log("API not response!"); } )
+                          .done( function( new_point ) { 
+                            // iso8601 to integer(time)
+                            // var date = new Date();
+                            // date = Date(new_point.x);
+                            // var x = new Date(date).getTime();
+
+                            x = (new Date()).getTime();
+
+                            series.addPoint([ x, new_point.y ]);
+                            
+                            series_length = series.points.length;
+                            last_point    = series.points[series_length - 1];
+                            latest_point  = series.points[series_length - 2];
+
+                            latest_point.update({ marker: { enabled: false } });
+                              last_point.update({ marker: { enabled:  true } });
+
+                            // Update currency
+                            difference = last_point.y - latest_point.y;
+                            if (difference < 0) { color = '#c00'; }
+                            $("#current").text( last_point.y );
+                            $("#difference").text( difference );
+                            $("#difference").css({ color: color });
+                          });
+                      }, #{5.seconds * 1000});
+                    }|.js_code
+              }
+
+      f.title text: 'Статистика количества игроков в игре Шаранавты', x: -20
+      f.subtitle text: 'За последние сутки!', x: -20
+
+      f.xAxis type: :datetime, tick_interval: 1.hour.to_i / 2 * 1000
+      f.yAxis title: {text: 'Количество шариков'}, plot_lines: [ value: 0, width: 1, color: '#808080']
+
+      f.tooltip value_suffix: ' шарик(ов)'
+      f.legend enabled: false
+
+      f.series name: 'Шарики', data: data, color: '#09f'
+      f.plot_options area: {
+                  fill_color: { linear_gradient: { x1: 0, y1: 0, x2: 0, y2: 1}, stops: [[0, '#09f'],[1, '#0df']] },
+                  line_width: 1,
+                  marker: {
+                    fillColor: "#93c",
+                    radius: 6,
+                    enabled: false
+                  },
+                  shadow: false,
+                  states: { hover: { line_width: 1 } },
+                  threshold: nil
+              }
+    end
+  end
+
+  def self.game_online1 # all works
     from = 24.hours.ago
     to   = Time.now
 
@@ -71,7 +164,7 @@ class Chart
               }
 
       f.title text: 'Статистика количества игроков в игре Шаранавты', x: -20
-      f.subtitle text: 'За последние сутки!', x: -20
+      # f.subtitle text: 'За последние сутки!', x: -20
 
       f.xAxis type: :datetime, tick_interval: 1.hour.to_i / 2 * 1000
       f.yAxis title: {text: 'Количество шариков'}, plot_lines: [ value: 0, width: 1, color: '#808080']
@@ -80,87 +173,6 @@ class Chart
       f.legend enabled: false
 
       f.series name: 'Шарики', data: data, color: '#09f'
-      f.plot_options area: {
-                  fill_color: { linear_gradient: { x1: 0, y1: 0, x2: 0, y2: 1}, stops: [[0, '#09f'],[1, '#0df']] },
-                  line_width: 1,
-                  marker: {
-                    fillColor: "#93c",
-                    radius: 6,
-                    enabled: false
-                  },
-                  shadow: false,
-                  states: { hover: { line_width: 1 } },
-                  threshold: nil
-              }
-    end
-  end
-
-  def self.game_online1 # all works
-    from = 16.hours.ago # Date.today.beginning_of_day
-    to   = Time.now
-
-    game_online = Statistic.select(from: from, to: to)
-
-    return nil unless game_online
-
-    LazyHighCharts::HighChart.new :game_online do |f|
-      f.chart type: :area,
-              zoom_type: :x,
-              animation: 'Highcharts.svg',
-              events: {
-                load: %|
-                    function() {
-                      var series = this.series[0],
-                          chart = this,
-                          color = '#0a0',
-                          series_length = series.points.length,
-                          last_point    = series.points[series_length - 1],
-                          latest_point  = series.points[series_length - 2],
-                          difference = last_point.y - latest_point.y;
-
-                      // Update currency
-                      if (difference < 0) { color = '#c00'; }
-                      $("#current").text( last_point.y );
-                      $("#difference").text( difference );
-                      $("#difference").css({ color: color });
-
-                      // Mark last point
-                      last_point.update({ marker: { enabled: true  } });
-
-                      setInterval(function() {
-                        color = '#0a0';
-                        difference = 0;
-
-                        $.getJSON("/new_dot", function(new_point) {
-                          series.addPoint([new_point.x , new_point.y]);
-
-                          series_length = series.points.length;
-                          last_point    = series.points[series_length - 1];
-                          latest_point  = series.points[series_length - 2];
-
-                          latest_point.update({ marker: { enabled: false } });
-                            last_point.update({ marker: { enabled:  true } });
-
-                          difference = last_point.y - latest_point.y;
-                          if (difference < 0) { color = '#c00'; }
-
-                          $("#current").text( last_point.y );
-                          $("#difference").text( difference );
-                          $("#difference").css({ color: color });
-                        });
-                      }, #{1.minute * 1000});
-                    }|.js_code
-              }
-
-      f.title text: 'Статистика количества игроков в игре Шаранавты', x: -20
-
-      f.xAxis type: :datetime, tick_interval: 1.hour.to_i / 2 * 1000
-      f.yAxis title: {text: 'Количество шариков'}, plot_lines: [ value: 0, width: 1, color: '#808080']
-
-      f.tooltip value_suffix: ' шарик(ов)'
-      f.legend enabled: false
-
-      f.series name: 'Шарики', data: game_online, color: '#09f'
       f.plot_options area: {
                   fill_color: { linear_gradient: { x1: 0, y1: 0, x2: 0, y2: 1}, stops: [[0, '#09f'],[1, '#0df']] },
                   line_width: 1,
