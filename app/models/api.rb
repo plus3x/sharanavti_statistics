6ef695b389a9d6a520874b4b47538085994b400b
plus3x <electronicchest@gmail.com>
2014-01-31 18:20:15 +0400

    Precompile assets

---
+++ app/models/api.rb
@@ -0,0 +1,18 @@
+class API < ActiveResource::Base
+  def self.game_online_select(args)
+      response = post( 'stat.online.get', date_from: args[:from], date_to: args[:to] )
+
+      game_online = YAML::load( response.body.gsub!(/(\,)(\S)/, "\\1 \\2") ) # get array form json
+    rescue
+     nil
+  end
+
+  def self.game_online_dot
+      response = post( 'stat.online.get', date_from: 2.minutes.ago, date_to: Time.now )
+
+      dots = YAML::load( response.body.gsub!(/(\,)(\S)/, "\\1 \\2") ) # get array form json
+      dots.last
+    rescue
+     nil
+  end
+end
