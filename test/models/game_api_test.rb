2601a449248c330fc3c4f669ab6b44f44e94d32c
plus3x <electronicchest@gmail.com>
2014-02-11 14:19:14 +0400

    Commit ActiveResources Mock

--- test/models/game_api_test.rb
+++ test/models/game_api_test.rb
@@ -5,18 +5,18 @@ class GameAPITest < ActiveSupport::TestCase
     @from = Date.today.beginning_of_day
     @to   = Time.now
 
-    @data = []
-    (@from.to_i..@to.to_i).step(1.minutes) { |time| @data << Random.rand(0..100) }
+    # @data = []
+    # (@from.to_i..@to.to_i).step(1.minutes) { |time| @data << Random.rand(0..100) }
 
-    ActiveResource::HttpMock.respond_to do |mock|
-      mock.get "/api/stat.online.get?#{ { date_from: @from, date_to: @to }.to_param }", {}, @data
-    end
+    # ActiveResource::HttpMock.respond_to do |mock|
+    #   mock.get "/api/stat.online.get?#{ { date_from: @from, date_to: @to }.to_param }", {}, @data
+    # end
   end
 
   test "should get game online select" do
     data = GameAPI.game_online_select( from: @from, to: @to )
 
-    puts data
+    # puts data
 
     assert_not_nil data
   end
